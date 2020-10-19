4531b130e1f84922d1e2f4ddf6bc83ce2e0eab36
siddharth <siddharth@Rmac.attlocal.net>
2020-10-18 19:45:16 -0700

    I made a silly code correctness error

--- src/data-containers/trie.js
+++ src/data-containers/trie.js
@@ -8,22 +8,29 @@ class Node {
 class Trie {
 
     constructor() {
-        this.root = new Node(terminal = false);
+        this.root = new Node(false);
     }
 
     add(word) {
         let curr_node = this.root;
         for (let i = 0; i < word.length; i++) {
-            let next_node = new Node(terminal = (i == word.length - 1));
-            curr_node.children.set(word[i], next_node);
+            let next_node;
+            if (curr_node.children.has(word[i])) {
+                next_node = curr_node.children.get(word[i]);
+            }
+            else {
+                next_node = new Node(i == word.length - 1);
+                curr_node.children.set(word[i], next_node);
+            }
+            curr_node = next_node;
         }
     }
 
     find(word) {
         let curr_node = this.root;
         for (let i = 0; i < word.length; i++) {
-            if (curr_node.has(word[i])) {
-                curr_node = curr_node.get(word[i]);
+            if (curr_node.children.has(word[i])) {
+                curr_node = curr_node.children.get(word[i]);
             }
             else {
                 return(false);
